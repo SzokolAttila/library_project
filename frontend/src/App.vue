5081acda734c32b37d42157f218de1e3edfaf3ca
B4lint0413 <balu050413@gmail.com>
2025-02-11 15:56:01 +0000

    created store for authors in frontend but not tested yet

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,11 @@
 <script>
 import {useGenreStore} from '@stores/GenreStore.mjs'
+import {useAuthorStore} from '@stores/AuthorStore.mjs'
 
 export default{
   async mounted(){
     await useGenreStore().getGenres();
+    await useAuthorStore().getAuthors();
   }
 }
 </script>
---
+++ frontend/src/stores/AuthorStore.mjs
@@ -0,0 +1,29 @@
+import { defineStore } from 'pinia'
+import {http} from '@utils/http.mjs'
+
+export const useAuthorStore = defineStore('author-store', {
+  state() {
+    return {
+      authors: []
+    }
+  },
+  actions: {
+    async getAuthors() {
+      let resp = http.get('authors');
+      this.authors = resp.data.data;
+    },
+    async getAuthor(id){
+      let resp = http.get(`authors/${id}`);
+      return resp.data.data;
+    }
+  },
+  getters: {
+    authorOptions() {
+      var options = {};
+      for(let author of this.authors){
+        options[author.id] = author.name;
+      }
+      return options;
+    }
+  }
+})
