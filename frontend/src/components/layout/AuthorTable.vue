7e9638d9bacaaad2a67f439508431bf9070b6b5d
B4lint0413 <balu050413@gmail.com>
2025-02-11 16:40:12 +0000

    created all authors page with table

---
+++ frontend/src/components/layout/AuthorTable.vue
@@ -0,0 +1,35 @@
+<template>
+    <table>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Nationality</th>
+                <th>Birth Date</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="author of authors" class="odd:bg-orange-400 even:bg-orange-100">
+                <td>{{ author.name }}</td>
+                <td>{{ author.nationality }}</td>
+                <td>{{ author.birth_date }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+import {mapActions, mapState} from 'pinia'
+import {useAuthorStore} from '@stores/AuthorStore.mjs'
+
+export default{
+    computed:{
+        ...mapState(useAuthorStore, ['authors'])
+    },
+    methods:{
+        ...mapActions(useAuthorStore, ['getAuthors'])
+    },
+    async mounted(){
+        await this.getAuthors();
+    }
+}
+</script>
---
+++ frontend/src/pages/authors/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <BaseLayout>
+        <h1 class="text-4xl m-4 font-bold">All Authors</h1>
+        <AuthorTable class="w-[80%] mx-auto"></AuthorTable>
+    </BaseLayout>
+</template>
+
+<script>
+import AuthorTable from '@components/layout/AuthorTable.vue';
+import BaseLayout from '@layouts/BaseLayout.vue'
+
+export default{
+    components:{
+        AuthorTable,
+        BaseLayout
+    }
+}
+</script>
+
+<route lang="json">
+{
+    "name": "authors" 
+}
+</route>
