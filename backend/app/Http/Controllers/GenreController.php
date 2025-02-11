6060e66f21259e48b172302672cc74f7fda653c5
B4lint0413 <balu050413@gmail.com>
2025-02-11 15:20:39 +0000

    created controller for genre

--- backend/app/Http/Controllers/GenreController.php
+++ backend/app/Http/Controllers/GenreController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreGenreRequest;
 use App\Http\Requests\UpdateGenreRequest;
+use App\Http\Resources\GenreResource;
 use App\Models\Genre;
 
 class GenreController extends Controller
@@ -13,38 +14,18 @@ class GenreController extends Controller
      */
     public function index()
     {
-        //
+        return GenreResource::collection(Genre::with('books')->get());
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreGenreRequest $request)
-    {
-        //
-    }
 
     /**
      * Display the specified resource.
      */
     public function show(Genre $genre)
     {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateGenreRequest $request, Genre $genre)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Genre $genre)
-    {
-        //
+        return new GenreResource($genre->load('books'));
     }
 }
