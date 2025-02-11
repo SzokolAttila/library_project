f05a2c0106d34364651543d4af3824e3c1386cda
B4lint0413 <balu050413@gmail.com>
2025-02-11 15:20:07 +0000

    modified models for joining

--- backend/app/Models/Book.php
+++ backend/app/Models/Book.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Book extends Model
 {
     use HasFactory;
+    public $timestamps = false;
+
+    protected $fillable = ['isbn', 'title', 'language', 'pages', 'published_date', 'description', 'genre_id', 'author_id', 'publisher_id'];
+
+    public function genre() : BelongsTo{
+        return $this->belongsTo(Genre::class);
+    }
+
+    public function publisher() : BelongsTo{
+        return $this->belongsTo(Publisher::class);
+    }
+
+    public function author() : BelongsTo{
+        return $this->belongsTo(Author::class);
+    }
 }
