92a6200ec0fded4b87eaa6f077d582eddf5cddeb
B4lint0413 <balu050413@gmail.com>
2025-02-11 15:21:19 +0000

    set up routing for genres and books

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,13 +1,13 @@
 <?php
 
 use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\GenreController;
 use App\Http\Controllers\PublisherController;
+use App\Http\Resources\BookResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::apiResource("publishers", PublisherController::class)->only(["index", "show"]);
 Route::apiResource("authors", AuthorController::class)->only(["index", "show"]);
+Route::apiResource("genres", GenreController::class)->only(["index", "show"]);
+Route::apiResource("books", BookResource::class);
